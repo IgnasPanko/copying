ef488e2b168920557303b963eb991ca0a12219ce
IgnasPanko <ignasklep@gmail.com>
2026-04-28 11:16:54 +0300

    prac 6

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,7 @@
-export async function AuthNav() {
-  return <ul className="grid grid-flow-col w-fit gap-x-2"></ul>
+export function Footer() {
+  return (
+    <footer className="mt-5 p-1 bg-black text-white text-sm text-center">
+      Copyright &copy; Ignas Klepšys
+    </footer>
+  )
 }
